bb880a0f8c5998e3207691b62132d27954651eb9
Konstantinos Theologitis <kth@xolta.com>
2021-07-29 19:24:39 +0200

    new approach & better html structure with proper elements

--- nav-logic.js
+++ nav-logic.js
@@ -3,4 +3,5 @@ let navMenu = document.getElementById("popup-menu");
 const toggleMenu = () => {
     let currentDisplayVal = window.getComputedStyle(navMenu).getPropertyValue('display');
     navMenu.style.display = currentDisplayVal === "none" ? "flex" : "none";
+    console.log(currentDisplayVal);
 }
